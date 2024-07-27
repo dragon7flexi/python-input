64042f4cc923c6ab704e1affbd1c9fb501d1fb90
dragon7flexi <dragon7.flexi@gmail.com>
2024-07-27 17:14:36 +0900

    fc

---
+++ python_input.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+a = [int(x) for x in input().split()]
+a = list(map(int, input().split()))
+a = [*map(int, input().split())]
+
+a = [int(input()) for _ in range(n)]
+
+# two-dimentional array
+a = [list(map(int, input().split(" "))) for _ in range(n)]
+
+s = list(input())
+
+s = [input() for _ in range(n)]
+
+
